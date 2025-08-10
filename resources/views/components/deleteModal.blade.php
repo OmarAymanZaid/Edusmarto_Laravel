79885982a2088cad35370237893b3d84ba0ba6fb
OmarAyman <omaraymanzaid1122@gmail.com>
2025-08-10 09:24:59 +0300

    Enhancing the delete modal design

--- resources/views/components/deleteModal.blade.php
+++ resources/views/components/deleteModal.blade.php
@@ -3,8 +3,8 @@
     <div class="modal-box">
         <p id="modalMessage">Are you sure you want to delete this item?</p>
         <div class="modal-buttons">
-            <button class="btn-cancel" id="modalCancel">Cancel</button>
             <button class="btn-confirm" id="modalConfirm">Confirm</button>
+            <button class="btn-cancel" id="modalCancel">Cancel</button>
         </div>
     </div>
 </div>
